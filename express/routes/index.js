0d07239e0da4dbc2e277ac823f9d82d4da60a9e1
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2018-04-04 04:27:50 +0800

    Aurelia.js Production build

--- express/routes/index.js
+++ express/routes/index.js
@@ -6,9 +6,9 @@ const path = require('path');
 router.get('/scripts/:filename', (req, res, next) => res.sendFile(path.join(__dirname, `../../aurelia/scripts/${req.params.filename}`)));
 
 router.get('/', (req, res, next) => {
-  let date = new Date();
-  date.setDate(date.getDate() + 1);
-  res.cookie('ipinfo', process.env.IPINFO, { expires: date, path: '/' });
+  // let date = new Date();
+  // date.setDate(date.getDate() + 1);
+  // res.cookie('ipinfo', process.env.IPINFO, { expires: date, path: '/' });
   // res.cookie('ipinfo', process.env.IPINFO, { expires: date, path: '/', secure: true });
   res.render('index', { title: 'Express' });
 });
